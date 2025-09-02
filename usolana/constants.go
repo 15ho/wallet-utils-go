ffa75dea13c41a2c174aeb133706ba82a01d91f8
15ho <fifteenho@163.com>
2025-09-02 23:20:44 +0800

    feat: solana transaction priority fee

--- usolana/constants.go
+++ usolana/constants.go
@@ -1,6 +1,7 @@
 package usolana
 
 const (
-	LamportsPerSOL uint64 = 1000000000 // 1 SOL
-	SOLDecimals    uint8  = 9
+	LamportsPerSOL      uint64 = 1000000000               // 1 SOL
+	MicroLamportsPerSOL uint64 = 1000000 * LamportsPerSOL // 1 SOL
+	SOLDecimals         uint8  = 9
 )
