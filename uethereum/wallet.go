bd828e6ce03084acdcefbd2494cf8bcae60f358a
15ho <fifteenho@163.com>
2025-09-09 14:18:28 +0800

    feat: ethereum transfer eth

--- uethereum/wallet.go
+++ uethereum/wallet.go
@@ -9,6 +9,7 @@ import (
 	"github.com/ethereum/go-ethereum"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
+	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 )
@@ -49,12 +50,50 @@ func NewWalletClient(endpoint, privateKeyHex string) (*WalletClient, error) {
 	}, nil
 }
 
-func (wc *WalletClient) EstimateGasTransferETH(ctx context.Context, to string, value *big.Int) (gas uint64, err error) {
+func (wc *WalletClient) EstimateGasTransferETH(ctx context.Context, to string, amount *big.Int) (gas uint64, err error) {
 	toAddr := common.HexToAddress(to)
 	gas, err = wc.cli.EstimateGas(ctx, ethereum.CallMsg{
 		From:  wc.account,
 		To:    &toAddr,
-		Value: value,
+		Value: amount,
 	})
 	return
+}
+
+func (wc *WalletClient) SuggestGasPrice(ctx context.Context) (gasPrice *big.Int, err error) {
+	return wc.cli.SuggestGasPrice(ctx)
+}
+
+func (wc *WalletClient) TransferETH(ctx context.Context, to string, amount *big.Int, gasLimit uint64, gasPrice *big.Int) (txHash string, err error) {
+	nonce, err := wc.cli.PendingNonceAt(ctx, wc.account)
+	if err != nil {
+		err = fmt.Errorf("get nonce: %v", err)
+		return
+	}
+	chainID, err := wc.cli.ChainID(ctx)
+	if err != nil {
+		err = fmt.Errorf("get chain id: %v", err)
+		return
+	}
+	tx := types.NewTransaction(nonce, common.HexToAddress(to), amount, gasLimit, gasPrice, nil)
+	tx, err = types.SignTx(tx, types.NewEIP155Signer(chainID), wc.privateKey)
+	if err != nil {
+		err = fmt.Errorf("sign tx: %v", err)
+		return
+	}
+
+	err = wc.cli.SendTransaction(ctx, tx)
+	if err != nil {
+		return
+	}
+	txHash = tx.Hash().Hex()
+	return
+}
+
+func (wc *WalletClient) GetETHBalance(ctx context.Context) (balance *big.Int, err error) {
+	return wc.cli.BalanceAt(ctx, wc.account, nil)
+}
+
+func (wc *WalletClient) GetETHBalanceByAddress(ctx context.Context, address string) (balance *big.Int, err error) {
+	return wc.cli.BalanceAt(ctx, common.HexToAddress(address), nil)
 }
--- uethereum/wallet_test.go
+++ uethereum/wallet_test.go
@@ -13,13 +13,36 @@ func TestCreateWalletAccount(t *testing.T) {
 	t.Logf("privateKeyHex: %s, address: %s", privateKeyHex, address)
 }
 
-func TestEstimateGasTransferETH(t *testing.T) {
+func TestWalletClient(t *testing.T) {
 	wc, err := NewWalletClient(EthTestnet, Acc1PrivateKeyHex)
 	assert.NoError(t, err)
 	ctx := t.Context()
 
-	value, _ := big.NewInt(0).SetString("1000000000000", 10)
-	gas, err := wc.EstimateGasTransferETH(ctx, Acc2AccountAddress, value)
-	assert.NoError(t, err)
-	t.Logf("gas: %d", gas)
+	t.Run("estimate gas transfer eth", func(t *testing.T) {
+		value, _ := big.NewInt(0).SetString("1000000000000", 10)
+		gas, err := wc.EstimateGasTransferETH(ctx, Acc2AccountAddress, value)
+		assert.NoError(t, err)
+		t.Logf("gas: %d", gas)
+	})
+
+	t.Run("transfer eth", func(t *testing.T) {
+		gasPrice, err := wc.SuggestGasPrice(ctx)
+		assert.NoError(t, err)
+		amount := new(big.Int).Div(WeiPerETH, big.NewInt(1000000)) // 0.000001 ETH
+		txHash, err := wc.TransferETH(ctx, Acc2AccountAddress, amount, 100000, gasPrice)
+		assert.NoError(t, err)
+		t.Logf("txHash: %s", txHash)
+	})
+
+	t.Run("get eth balance", func(t *testing.T) {
+		balance, err := wc.GetETHBalance(ctx)
+		assert.NoError(t, err)
+		t.Logf("acc1 eth balance: %s", balance)
+	})
+
+	t.Run("get eth balance by address", func(t *testing.T) {
+		balance, err := wc.GetETHBalanceByAddress(ctx, Acc2AccountAddress)
+		assert.NoError(t, err)
+		t.Logf("acc2 eth balance: %s", balance)
+	})
 }
